ea226c1a9ab4531f6d060a4329534dfe82d02ba5
Ryane Chia <ryanechia@outlook.com>
2020-09-28 00:38:20 +0800

    add models

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+class Portfolio:
+    def __init__(self, name, amount, risk):
+        self.name = name
+        self.amount = amount
+        self.risk = risk
+
+
+class DepositPlan:
+    def __init__(self, deposits, recurrence, completed):
+        self.deposits = deposits
+        self.recurrence = recurrence
+        self.completed = completed
+
+
+class Deposit:
+    def __init__(self, target_portfolio, amount, currency, ratio):
+        self.target_portfolio = target_portfolio
+        self.amount = amount
+        self.currency = currency,
+
+
+class User:
+    def __init__(self, name, portfolios, deposit_plans):
+        self.name = name
+        self.portfolios = portfolios
+        self.deposit_plans = deposit_plans
+
+
 def main():
     print('hello world')
 
